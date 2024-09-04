4953d2622aacfb681d048ea02d80c46f879680f3
Ajani Small <159090147+AJANI2005@users.noreply.github.com>
2024-09-04 16:28:44 -0400

    first commit

---
+++ JavaBasics/Basics.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class Basics {
+    public static void main(String[] args) {
+       
+        Scanner scan = new Scanner(System.in);
+        String num = scan.nextLine();
+        System.out.println(num);
+    }
+
+ 
+}
+
